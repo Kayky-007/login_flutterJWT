42d4a77bfbb49626887762f00b4d7720860a4e50
Kayky <149194790+Kayky-007@users.noreply.github.com>
2024-10-17 13:41:14 -0300

    Finalização

--- lib/controller/medicos_controller.dart
+++ lib/controller/medicos_controller.dart
@@ -1,45 +1,17 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:medicos_jwt/model/medico_model.dart';
+
 class MedicosController {
   String? token;
-FlutterSecureStorage storage = FlutterSecureStorage();
+  FlutterSecureStorage storage = FlutterSecureStorage();
+ 
+  Future<List<Dados>?>? obterDadosMedicos() async {
+    token = await storage.read(key: 'token');
+        MedicosModal medicosModal = await MedicosModal.dadosMedicos(token);
 
-
-Future<void> obterDadosMedicos() async {
-  MedicosModal medicosModal = await MedicosModal.dadosMedicos(token);
-  token = medicosModal.token;
-  await storage.write(key: 'token', value: token );
-  print(token);
-
-  
-  
+      await storage.write(key: 'token', value: medicosModal.token);
+    
+    
+    return medicosModal.dados;
   }
-
-
-
-
-
-
-// Future<MedicosModal> obterDadosMedicos() async {
-
-    
-//    token = await storage.read(key: 'token');
-//     // Acessa o token
-    
-//     print("Token: $token");
-
-//     List<Dados>? listaDeMedicos = medicos.dados;
-
-//     if (listaDeMedicos != '') {
-//         print("Nome: ${listaDeMedicos![0].nome}");
-//         print("Telefone: ${listaDeMedicos![0].telefone}");
-//         print("Especialidade: ${listaDeMedicos![0].especialidade}");
-//         print("Foto URL: ${listaDeMedicos[0].fotoUrl}");
-//     } else {
-//       print("Nada encontrado");
-//     }
-
-// }
-
 }
--- lib/views/medicos_view.dart
+++ lib/views/medicos_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:jwt_decoder/jwt_decoder.dart';
 import 'package:medicos_jwt/controller/medicos_controller.dart';
+import 'package:medicos_jwt/model/medico_model.dart';
 
 class MedicosView extends StatefulWidget {
   const MedicosView({super.key});
@@ -12,108 +11,29 @@ class MedicosView extends StatefulWidget {
 
 class _MedicosViewState extends State<MedicosView> {
   MedicosController _controller = MedicosController();
-  final storage = FlutterSecureStorage();
-
-  // Future<String?>? teste;
-
-  // Obter dados:
-  Future<void> inicializador() async {
-    _controller.token = await storage.read(key: 'token');
-    // verificação se o token expirou
-    if (_controller.token == '' || JwtDecoder.isExpired(_controller.token!)) {
-      Navigator.of(context).pushReplacementNamed('/login');
-    }
-    print(_controller.token);
-    print(JwtDecoder.isExpired(_controller.token!));
-    print(JwtDecoder.decode(_controller.token!));
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    inicializador();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Medicos JWT'),
+        title: const Text('Resultado'),
       ),
-
-      body: Column(
-        children: [
-          Text('Clique abaixo:'),
-          SizedBox(
-            height: 30,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                  onPressed: () {
-                    _controller.obterDadosMedicos();
-                    
-                  },
-                  child: Text('Procurar')),
-            ],
-          ),
-          Row(
-            children: [
-              Card(
-                elevation: 5, // Sombra
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child:  Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: Column(
-                    children: [
-                      Text(
-                        'Nome: ',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        ),
-                      ),
-                      Text(
-                        'Telefone: ',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        ),
-                      ),
-                      Text(
-                        'Especialidade: ',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        ),
-                      ),
-                      Image.network('http://pin0581814w10-1.senacsp.edu.br:3010/imagens/600.jpg',
-                      width: 200,)
-                    ],
-                  ),
-                ),
-              )
-            ],
-          ),
-        ],
+      body: FutureBuilder<List<Dados>?>(
+        future: _controller.obterDadosMedicos(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          return ListView.builder(
+            itemCount: snapshot.data?.length ?? 0,
+            itemBuilder: (context, index) {
+             
+             return Text('Nome: ${snapshot.data![index].nome}');
+              
+            },
+          );
+        },
       ),
-
-      // body: FutureBuilder(
-
-      //   builder: (context, snapshot) {
-      //     if (snapshot.connectionState == ConnectionState.waiting) {
-      //       return const CircularProgressIndicator();
-      //     } else {
-      //       return Text(snapshot.data ?? 'nada');
-      //     }
-      //   },
-      // ),
     );
   }
 }
